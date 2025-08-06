062a48d39d8523c48c7c03f03006c5ba073ec1fc
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-08-06 03:34:57 +0300

    p2

--- Assignment12_LINQ/Program.cs
+++ Assignment12_LINQ/Program.cs
@@ -14,13 +14,30 @@ namespace Assignment12_LINQ
 
             #region p1: Find all products that are out of stock.
 
-            var products = ListGenerators.ProductList;
+            //var products = ListGenerators.ProductList;
 
-            var outOfStockProducts = products.Where(p => p.UnitsInStock == 0);
+            //var outOfStockProducts = products.Where(p => p.UnitsInStock == 0);
 
-            foreach (var product in outOfStockProducts)
+            //foreach (var product in outOfStockProducts)
+            //{
+            //    Console.WriteLine($"{product.ProductName} is out of stock.");
+            //}
+
+            #endregion
+
+
+
+            #region p2: Find all products that are in stock and cost more than 3.00 per unit.
+
+            List<Product> products = ListGenerators.ProductList;
+
+            var result = products.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3.00m);
+
+            Console.WriteLine("Products in stock and cost more than $3.00:");
+
+            foreach (var product in result)
             {
-                Console.WriteLine($"{product.ProductName} is out of stock.");
+                Console.WriteLine($"- {product.ProductName}, Price: {product.UnitPrice:C}, Stock: {product.UnitsInStock}");
             }
 
             #endregion
