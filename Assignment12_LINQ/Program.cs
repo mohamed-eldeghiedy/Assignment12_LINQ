96c52466b7385ddaf877a477117a4518f4294db4
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-08-06 03:27:15 +0300

    p1

--- Assignment12_LINQ/Program.cs
+++ Assignment12_LINQ/Program.cs
@@ -10,8 +10,21 @@ namespace Assignment12_LINQ
         static void Main()
         {
 
-             
-            
+            // q1 : Restriction Operators
+
+            #region p1: Find all products that are out of stock.
+
+            var products = ListGenerators.ProductList;
+
+            var outOfStockProducts = products.Where(p => p.UnitsInStock == 0);
+
+            foreach (var product in outOfStockProducts)
+            {
+                Console.WriteLine($"{product.ProductName} is out of stock.");
+            }
+
+            #endregion
+
         }
     }
 }
